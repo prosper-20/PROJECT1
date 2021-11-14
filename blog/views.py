18d4d293af3924499c75a5fa575dad546b240478
prosper-20 <edwardprosper001@gmail.com>
2021-11-15 00:30:48 +0100

    Updates

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 def home(request):
-    posts = [
-        {
-            "author": "Edward Prosper",
-            "content": "First Blog Post",
-            "date_posted": "November 13, 2021",
-            "title": "Blog Post 1"
-        },
-        {
-            "author": "Deborah Iluonaze",
-            "content": "Second Blog Post",
-            "date_posted": "June 13, 2021",
-            "title": "Blog Post 2"
-        },
-    ]
 
     context = {
-        "posts": posts,
+        "posts": Post.objects.all(),
     }
 
     return render(request, "blog/home_1.html", context)
