1b23cd21f6c94a3a4abc0abb9fa8375fc7962f79
prosper-20 <edwardprosper001@gmail.com>
2021-11-15 23:20:15 +0100

    HTML

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,21 @@ from django.urls import path
 from . import views
 from .views import (
     PostListView,
-    PostDetailView
+    PostDetailView,
+    PostCreateView, 
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
 )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path("about/", views.about, name='about'),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail" )
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail" ),
+    path('post/new/', PostCreateView.as_view(), name="post_create"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post_update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post_delete"),
+    path("user/<str:username>/", UserPostListView.as_view(), name="user_posts"),
+
+    
 ]
