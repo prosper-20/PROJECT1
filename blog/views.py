6c370eeed21aa8eb0ba32489bec3dcfab930a000
prosper-20 <edwardprosper001@gmail.com>
2021-11-15 19:42:16 +0100

    Updates

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,17 @@ def home(request):
         "posts": Post.objects.all(),
     }
 
-    return render(request, "blog/home_1.html", context)
+    return render(request, "users/home-page.html", context)
 
 def about(request):
     return render(request, "blog/about.html")
+
+
+def search_posts(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        # This returns the results of the user's search
+        posts = Post.objects.filter(title__contains=searched)
+        return render(request, "blog/new_search_posts.html", {'searched': searched, 'posts': posts})
+    else:
+        return render(request, "blog/new_search_posts.html")
