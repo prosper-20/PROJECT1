bc9d418e17dd6fcfa0b53939d56a6d3652cded6b
prosper-20 <edwardprosper001@gmail.com>
2021-11-16 23:16:56 +0100

    Further Updates

--- PROJECT1/settings.py
+++ PROJECT1/settings.py
@@ -137,3 +137,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "/"
+
+LOGIN_URL = 'login'
+
+
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'edwardprosper001@gmail.com'
+EMAIL_HOST_PASSWORD = 'cristianoronaldoisdgr8st'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'edwardprosper001@gmail.com'
+EMAIL_BACKEND = "sgbackend.SendGridBackend"
--- PROJECT1/urls.py
+++ PROJECT1/urls.py
@@ -25,10 +25,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")),
     path("register/", user_views.register, name="register"),
-    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("login/", auth_views.LoginView.as_view(template_name="users/main_login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path("search_posts/", views.search_posts, name='search_posts'),
     path("profile/", user_views.profile, name="profile"),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"), name="password_reset"),
+    path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),
+    path("password_reset_complete/", auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name='password_reset_complete'),
     
 
 ]
