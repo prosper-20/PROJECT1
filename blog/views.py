2c7cdf533b8c70dee775027a9ce1716b99157928
prosper-20 <edwardprosper001@gmail.com>
2021-11-24 16:23:32 +0100

    Modifications

--- blog/views.py
+++ blog/views.py
@@ -136,13 +136,16 @@ def search_posts(request):
 class PostCommentView(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
-    success_url = "/"
+    # success_url = "/"
     template_name = "blog/post_comment_form.html"
 
     def form_valid(self, form):
         form.instance.name = self.request.user
         form.instance.post_id = self.kwargs['pk']
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['pk']})
 
 
     
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class TestPage(TestCase):
+    def test_home_page_works(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/home-page.html')
+        self.assertContains(response, 'HIS BLOG')
+
+    def test_about_us_page_works(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, 'blog/new_about.html')
