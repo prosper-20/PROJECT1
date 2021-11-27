b0894777e99860a4e6f92d6f040f9510e277c10c
prosper-20 <edwardprosper001@gmail.com>
2021-11-27 19:40:02 +0100

    Updated tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,6 @@
+from django.http import response
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -7,3 +9,9 @@ class TestPage(TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "users/home-page.html")
+
+    def post_form_works(self):
+        response = self.client.get(reverse("post_create"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/post_form.html")
+
