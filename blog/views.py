f74d83140f481831fea37fea27c1d0f019251645
prosper-20 <edwardprosper001@gmail.com>
2021-12-01 14:59:49 +0100

    Updated Views

--- blog/views.py
+++ blog/views.py
@@ -150,7 +150,7 @@ class PostCommentView(LoginRequiredMixin, CreateView):
 
     
 def PoliticsView(request):
-    posts = Post.objects.filter(category="P").all().order_by("-id")
+    posts = Post.objects.filter(category="5").all().order_by("-id")
     context = {
         "posts": posts
     }
@@ -158,30 +158,52 @@ def PoliticsView(request):
 
 
 def EntertainmentView(request):
-    posts = Post.objects.filter(category="E").all().order_by("-id")
+    posts = Post.objects.filter(category="4").all().order_by("-id")
     context = {
         "posts": posts
     }
     return render(request, "blog/entertainment_view.html", context)
 
+def LifeView(request):
+    posts = Post.objects.filter(category="1").all().order_by("-id")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/life_view.html", context)
+
+def CrimeView(request):
+    posts = Post.objects.filter(category="2").all().order_by("-id")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/crime_view.html", context)
+
+def EducationView(request):
+    posts = Post.objects.filter(category="6").all().order_by("-id")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/education_view.html", context)
+
+
 def TrendingView(request):
-    posts = Post.objects.filter(category="T").all().order_by("-id")
+    posts = Post.objects.filter(category="7").all().order_by("-id")
     context = {
         "posts": posts
     }
     return render(request, "blog/trending_view.html", context)
 
 
-def MusicView(request):
-    posts = Post.objects.filter(category="M").all().order_by("-id")
-    context = {
-        "posts": posts
-    }
-    return render(request, "blog/music_view.html", context)
+# def MusicView(request):
+#     posts = Post.objects.filter(category="M").all().order_by("-id")
+#     context = {
+#         "posts": posts
+#     }
+#     return render(request, "blog/music_view.html", context)
 
 
 def SportsView(request):
-    posts = Post.objects.filter(category="S").all().order_by("-id")
+    posts = Post.objects.filter(category="3").all().order_by("-id")
     context = {
         "posts": posts
     }
