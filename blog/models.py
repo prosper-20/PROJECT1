42b3fa577cd1e4f65cf6bf3edf9a5cdc283c183c
prosper-20 <edwardprosper001@gmail.com>
2021-12-01 14:02:14 +0100

    Changed The Category Model

--- blog/models.py
+++ blog/models.py
@@ -26,12 +26,20 @@ POST_CHOICES = (
 )
 
 
+class Category(models.Model): # < here
+    title = models.CharField(max_length=255, default='')
+    
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = RichTextField(blank=True, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    # category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    category = models.ForeignKey(Category, default="", on_delete=models.CASCADE)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     post_choices = models.CharField(choices=POST_CHOICES, max_length=1)
     image = models.ImageField(default="newpost.jpg", upload_to="default_pics")
