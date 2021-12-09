3fb56fa2571f854863584c6cddf1ca5a3ee5d487
prosper-20 <edwardprosper001@gmail.com>
2021-12-09 13:46:52 +0100

    mods

--- PROJECT1/settings.py
+++ PROJECT1/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("PROJECT_1_SECRET_KEY")
+SECRET_KEY = "django-insecure-&qcgd#vrx6xo+i1an&rnu#)jqzzt^txlo^mi)1l9@8r)2&md(d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
