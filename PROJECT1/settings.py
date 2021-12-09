c8cec802ddd7ed7c28afa2331722ed32fda1d9d9
prosper-20 <edwardprosper001@gmail.com>
2021-12-09 13:53:36 +0100

    updates

--- PROJECT1/settings.py
+++ PROJECT1/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&qcgd#vrx6xo+i1an&rnu#)jqzzt^txlo^mi)1l9@8r)2&md(d"
+SECRET_KEY = os.environ.get("NEW_PROJECT1_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
